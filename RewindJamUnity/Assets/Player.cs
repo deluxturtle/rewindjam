a61bbc6a54c45650b32a147bb6d103deef1ff265
deluxturtle <sebaturtle@gmail.com>
2020-08-01 13:29:37 -0500

    getting used to nav mesh

---
+++ RewindJamUnity/Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    
+    NavMeshAgent agent;
+    Vector3 origin = Vector3.zero;
+    float test;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        transform.position = origin;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
