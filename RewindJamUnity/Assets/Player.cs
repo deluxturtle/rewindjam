52f61a90cabcfb3db3a68e60469d12e75866d01f
deluxturtle <sebaturtle@gmail.com>
2020-08-02 17:32:42 -0500

    cool tank controls

--- RewindJamUnity/Assets/Player.cs
+++ RewindJamUnity/Assets/Player.cs
@@ -5,21 +5,58 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
-    
-    NavMeshAgent agent;
-    Vector3 origin = Vector3.zero;
-    float test;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        agent = GetComponent<NavMeshAgent>();
-        transform.position = origin;
+    public float sensitivity = 0.1f;
+    public int maxThrottle = 10;
+    public float smoothMov = 0.5f;
+    public float smoothTurning = 2f;
+    float leftThrottleValue = 0f;
+    float rightThrottleValue = 0f;
+    Rigidbody rigidbody;
+
+    private void Start() {
+        rigidbody = GetComponent<Rigidbody>();    
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void FixedUpdate() {
+        leftThrottleValue += Input.GetAxis("VerticalLeft") * sensitivity;
+        leftThrottleValue = (leftThrottleValue > maxThrottle) ? maxThrottle : leftThrottleValue;
+        float roundedLeftThrottle = Mathf.Round(leftThrottleValue);
+        if(Mathf.Abs(roundedLeftThrottle - leftThrottleValue) < 0.02f)
+        {
+            leftThrottleValue = roundedLeftThrottle;
+        }
+        else if(roundedLeftThrottle > leftThrottleValue)
+        {
+            leftThrottleValue += 0.01f;
+        }
+        else if(roundedLeftThrottle < leftThrottleValue)
+        {
+            leftThrottleValue -= 0.01f;
+        }
+
+        rightThrottleValue += Input.GetAxis("VerticalRight") * sensitivity;
+        rightThrottleValue = (rightThrottleValue > maxThrottle) ? maxThrottle : rightThrottleValue;
+        float roundedRightThrottle = Mathf.Round(rightThrottleValue);
+        if(Mathf.Abs(roundedRightThrottle - rightThrottleValue) < 0.02f)
+        {
+            rightThrottleValue = roundedRightThrottle;
+        }
+        else if(roundedRightThrottle > rightThrottleValue)
+        {
+            rightThrottleValue += 0.01f;
+        }
+        else if(roundedRightThrottle < rightThrottleValue)
+        {
+            rightThrottleValue -= 0.01f;
+        }
+
+        //move the tank
+        Vector3 movement = transform.forward * ((leftThrottleValue + rightThrottleValue)/2f) * smoothMov * Time.deltaTime;
+        rigidbody.MovePosition(rigidbody.position+movement);
+
+        //turn the tank
+        float turn = (leftThrottleValue - rightThrottleValue) * smoothTurning * Time.deltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+        rigidbody.MoveRotation(rigidbody.rotation * turnRotation);
     }
 }
