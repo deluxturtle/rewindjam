40f7103abe8981fc2786f04825fea5aa2d71dc9b
Andrew Seba <sebaturtle@gmail.com>
2020-08-04 20:16:38 -0500

    player movement

---
+++ RewindJamUnity/Assets/Scripts/OrbitCamera.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// https://www.youtube.com/watch?v=bVo0YLLO43s
+/// </summary>
+public class OrbitCamera : MonoBehaviour
+{
+
+    public float mouseSensitivity = 4f;
+    public float scrollSensitivity = 2f;
+    public float orbitDampening = 10f;
+    public float scrollDampening = 6f;
+    public bool cameraDisabled = false;
+
+    Transform cameraTransform;
+    Transform parentTransform;
+    Vector3 localRotation;
+    float cameraDist = 10f;
+
+    private void Start() {
+        cameraTransform = transform;
+        parentTransform = transform.parent;
+    }
+    private void LateUpdate() {
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+            cameraDisabled = !cameraDisabled;
+        if(!cameraDisabled)
+        {
+            //rotation of the camera based on the mouse coordinates
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+            if(mouseX != 0 || mouseY != 0)
+            {
+                localRotation.x += mouseX * mouseSensitivity;
+                localRotation.y -= mouseY * mouseSensitivity;
+
+                //clamp the y rotation to horizon
+                localRotation.y = Mathf.Clamp(localRotation.y, 0f, 90f);
+            }
+
+            //dolly
+            float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
+            if(scrollWheel != 0f)
+            {
+                float scrollAmount = scrollWheel * scrollSensitivity;
+                scrollAmount *= (cameraDist * 0.3f);
+                cameraDist += scrollAmount * -1f;
+                cameraDist = Mathf.Clamp(cameraDist, 1.5f, 100f);
+            }
+        }
+        Quaternion quaternion = Quaternion.Euler(localRotation.y, localRotation.x, 0);
+        parentTransform.rotation = Quaternion.Lerp(parentTransform.rotation, quaternion, Time.deltaTime * orbitDampening);
+        if(cameraTransform.localPosition.z != cameraDist * -1f)
+        {
+            cameraTransform.localPosition = new Vector3(0,0, Mathf.Lerp(cameraTransform.localPosition.z, cameraDist * -1f, Time.deltaTime * scrollDampening));
+        }
+    }
+}
---
+++ RewindJamUnity/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+
+    public float speeed = 5f;
+    float horizontal;
+    float vertical;
+    Rigidbody rigidbody;
+    private void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        
+
+    }
+
+    private void FixedUpdate() {
+        Vector3 targetDir = new Vector3(horizontal, 0, vertical);
+        
+        transform.position += targetDir * speeed * Time.deltaTime;
+    }
+
+}
